9ca09c07ba0c93c2e76a03458c1e2d2f9dcc8bf3
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2025-12-15 15:58:44 +0100

    | JSON pipe

---
+++ book-rating/src/app/books/dashboard-page/dashboard-page.ts
@@ -0,0 +1,35 @@
+import { Component, signal } from '@angular/core';
+import { Book } from '../shared/book';
+import { JsonPipe, UpperCasePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-dashboard-page',
+  imports: [JsonPipe, UpperCasePipe],
+  templateUrl: './dashboard-page.html',
+  styleUrl: './dashboard-page.scss',
+})
+export class DashboardPage {
+
+  // 🦆
+  readonly books = signal<Book[]>([
+    {
+      isbn: '000',
+      title: 'Angular',
+      description: 'Bestes Angular Buch überhaupt',
+      rating: 5
+    },
+    {
+      isbn: '111',
+      title: 'React',
+      description: 'Auch ganz nettes Buch',
+      rating: 3
+    },
+    {
+      isbn: '222',
+      title: 'jQuery',
+      description: 'Super altes Buch',
+      rating: 1
+    }
+  ]);
+
+}
