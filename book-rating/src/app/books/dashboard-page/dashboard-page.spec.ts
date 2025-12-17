67b28a3e11a65ce259359337c9d9d8898be5647a
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2025-12-17 10:37:22 +0100

    unit testing: klassische mocks

--- book-rating/src/app/books/dashboard-page/dashboard-page.spec.ts
+++ book-rating/src/app/books/dashboard-page/dashboard-page.spec.ts
@@ -1,14 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardPage } from './dashboard-page';
+import { BookRatingHelper } from '../shared/book-rating-helper';
+import { Book } from '../shared/book';
 
 describe('DashboardPage', () => {
   let component: DashboardPage;
   let fixture: ComponentFixture<DashboardPage>;
 
   beforeEach(async () => {
+
+    // Der ganz klassische Mock
+    const ratingMock = {
+      rateUp: (book: Book) => { console.log('Unter meiner Kontrolle:', book); return book }
+    }
+
     await TestBed.configureTestingModule({
-      imports: [DashboardPage]
+      imports: [DashboardPage],
+      providers: [
+        {
+          provide: BookRatingHelper,
+          useValue: ratingMock
+        }
+      ]
     })
     .compileComponents();
 
@@ -18,6 +32,11 @@ describe('DashboardPage', () => {
   });
 
   it('should create', () => {
+
+    console.log('HALLO');
+    component.doRateUp({} as Book);
+
     expect(component).toBeTruthy();
+
   });
 });
