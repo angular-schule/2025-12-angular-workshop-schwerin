ec40a0a05c70765f77d6edee9dea635cdc160051
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2025-12-18 16:23:20 +0100

    ngrx per hand, part 1

--- book-rating/src/app/app.config.ts
+++ book-rating/src/app/app.config.ts
@@ -1,17 +1,21 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withDebugTracing, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes,
-      withInMemoryScrolling({
+    provideRouter(routes, withInMemoryScrolling({
         scrollPositionRestoration: 'top',
-      }),
-      // withDebugTracing()
-      withComponentInputBinding()
-    )
-  ]
+    }),
+    // withDebugTracing()
+    withComponentInputBinding()),
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+]
 };
